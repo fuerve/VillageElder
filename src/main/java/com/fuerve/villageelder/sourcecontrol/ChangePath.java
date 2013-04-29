027691b3042de5a69ce04b72256742bbe4c9c4df
Lucas Parker <fuerve@yahoo.com>
2013-04-28 21:01:40 -0700

    Adding Apache license header to the source files.

--- src/main/java/com/fuerve/villageelder/sourcecontrol/ChangePath.java
+++ src/main/java/com/fuerve/villageelder/sourcecontrol/ChangePath.java
@@ -1,15 +1,30 @@
 /**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.fuerve.villageelder.sourcecontrol;
+
+/**
  * ChangePath - Represents a changed path entry in a revision.
  * A revision will often affect multiple files, implying a
  * one-to-many relationship between a revision and the paths
  * that took the change.  An object of this type is a single
  * such path within a revision, of which there may be many.
- */
-package com.fuerve.villageelder.sourcecontrol;
-
-/**
  * @author lparker
- *
  */
 public class ChangePath {
    private final String path;
--- src/main/java/com/fuerve/villageelder/sourcecontrol/SubversionRepository.java
+++ src/main/java/com/fuerve/villageelder/sourcecontrol/SubversionRepository.java
@@ -1,6 +1,20 @@
 /**
- * SubversionRepository - Concrete implementation of Repository that supports
- * the Subversion source control system via the SVNKit library.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.fuerve.villageelder.sourcecontrol;
 
@@ -25,6 +39,8 @@ import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 /**
+ * SubversionRepository - Concrete implementation of Repository that supports
+ * the Subversion source control system via the SVNKit library.
  * @author lparker
  *
  */
